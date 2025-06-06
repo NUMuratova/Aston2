93052bd80a6f7fceb943a836c4f014289209d2a6
NUMuratova <167119864+NUMuratova@users.noreply.github.com>
2025-06-06 17:19:52 +0300

    Update TestLesson2Junit.java

--- src/test/java/TestLesson2Junit.java
+++ src/test/java/TestLesson2Junit.java
@@ -5,31 +5,31 @@ public class TestLesson2Junit {
 
     @Test
     void testFactorial() {
-        assertEquals(FactorialCalculator.factorial(5), 120); // Проверка правильного значения факториала
+        assertEquals(FactorialCalculator.factorial(5), 120); 
         assertThrows(IllegalArgumentException.class,
-                () -> FactorialCalculator.factorial(-1)); // Ошибка для отрицательных значений
+                () -> FactorialCalculator.factorial(-1)); 
     }
 
     @Test
     void testTriangleArea() {
-        assertEquals(TriangleAreaCalculator.triangleArea(3, 4, 5), 6.0, 0.001); // Площади прямоугольного треугольника
+        assertEquals(TriangleAreaCalculator.triangleArea(3, 4, 5), 6.0, 0.001); 
         assertThrows(IllegalArgumentException.class,
-                () -> TriangleAreaCalculator.triangleArea(1, 1, 3)); // Неверная сторона
+                () -> TriangleAreaCalculator.triangleArea(1, 1, 3)); 
     }
 
     @Test
     void testArithmeticOperations() {
-        assertEquals(ArithmeticOperations.add(7, 8), 15);           // Правильное сложение
-        assertEquals(ArithmeticOperations.subtract(10, 3), 7);      // Правильное вычитание
-        assertEquals(ArithmeticOperations.multiply(5, 6), 30);       // Правильное умножение
+        assertEquals(ArithmeticOperations.add(7, 8), 15);
+        assertEquals(ArithmeticOperations.subtract(10, 3), 7);
+        assertEquals(ArithmeticOperations.multiply(5, 6), 30);
         assertThrows(ArithmeticException.class,
-                () -> ArithmeticOperations.divide(10, 0));     // Деление на ноль должно вызывать исключение
+                () -> ArithmeticOperations.divide(10, 0));
     }
 
     @Test
     void testCompareNumbers() {
-        assertEquals(NumberComparator.compareNumbers(5, 3), "Первое число больше второго");   // Первое больше
-        assertEquals(NumberComparator.compareNumbers(2, 8), "Второе число больше первого");   // Второе больше
-        assertEquals(NumberComparator.compareNumbers(4, 4), "Числа равны");                   // Числа равны
+        assertEquals(NumberComparator.compareNumbers(5, 3), "Первое число больше второго");
+        assertEquals(NumberComparator.compareNumbers(2, 8), "Второе число больше первого");
+        assertEquals(NumberComparator.compareNumbers(4, 4), "Числа равны");
     }
 }
